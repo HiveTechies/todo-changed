f846eaa2534d32b7ada683c7614d29d03859ead7
ynvp <54361730+YNVP@users.noreply.github.com>
2020-07-11 06:48:14 +0530

    Update check.py

--- todo/check.py
+++ todo/check.py
@@ -1,7 +1,4 @@
 
-# the sole purpose of this warning is to prevent people who have
-# django-autocomplete-light installed but not configured to start the app
-@register()
 def dal_check(app_configs, **kwargs):
     errors = []
     return errors
